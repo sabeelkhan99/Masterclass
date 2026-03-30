32ff2a8e00ca61d430cd7517777badf72ad40400
Sabeel Khan <sabeelhps@gmail.com>
2026-03-30 20:53:29 +0530

    react-ecommerce-30th-march

---
+++ react-ecommerce-mar-30/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AutoSuggestion from './components/AutoSuggestion'
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className='container'>
+            <h1>Welcome to react</h1>
+            <AutoSuggestion/>
+        </div>
+    )
+}
+
+export default App
---
+++ react-ecommerce-mar-30/src/components/AutoSuggestion.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+
+async function searchProducts(query) {
+    const res = await fetch(`https://dummyjson.com/products/search?q=${query}`);
+    const data = await res.json();
+    return data;
+}
+
+const AutoSuggestion = () => {
+
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        async function makeApiCall() {
+            const res = await searchProducts(query);
+            // Out of the response we are grabbing the products key and placing inside results
+            setResults(res.products);
+        }
+        makeApiCall();
+    }, [query]);
+
+    const inputChangeHandler = (event) => {
+        // whatever is being typed in input is set to `query`
+        setQuery(event.target.value);
+    }
+
+    return (
+        <div className='autosuggestion'>
+            <input onChange={inputChangeHandler} type="text" placeholder='Search' />
+            <section>
+                {
+                    results.map((item) => {
+                        return <article key={item.id} className='item'>
+                            <img src={item.thumbnail} alt={item.title} />
+                            <span>{ item.title }</span>
+                        </article>
+                    })
+                }
+            </section>
+        </div>
+    )
+}
+
+export default AutoSuggestion
